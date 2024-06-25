d4718794de73e501e3fc3634306745798c86fd8e
BrunoGuilhermeS <86699309+BrunoGuilhermeS@users.noreply.github.com>
2024-06-24 21:34:39 -0300

    Add files via upload

--- src/test/java/com/example/agregadorInvestimentos/service/UserServiceTest.java
+++ src/test/java/com/example/agregadorInvestimentos/service/UserServiceTest.java
@@ -14,12 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -32,6 +33,8 @@ class UserServiceTest {
 
     @Captor
     private ArgumentCaptor<User> userArgumentCaptor;
+    @Captor
+    private ArgumentCaptor<UUID> uuidArgumentCaptor;
 
     @Nested
     class createUser {
@@ -78,6 +81,121 @@ class UserServiceTest {
 
             //Act & Assert
             assertThrows(RuntimeException.class, () -> userService.createUser(input));
+
+        }
+    }
+
+    @Nested
+    class getUserById {
+
+        @Test
+        @DisplayName("Deve encontrar o usuario por ID com sucesso quando o optional está presente")
+        void shouldGetUserByIdWithSuccessWhenOptionalIsPresent() {
+
+            //Arrange
+
+            var user = new User(
+                    UUID.randomUUID(),
+                    "usernameteste",
+                    "teste@email.com",
+                    "senhateste123",
+                    Instant.now(),
+                    null
+            );
+
+            doReturn(Optional.of(user)).when(userRepository).findById(uuidArgumentCaptor.capture());
+
+            //Act
+
+            var output = userService.getUserById(user.getUserId().toString());
+
+            //Assert
+
+            assertTrue(output.isPresent());
+            assertEquals(user.getUserId(), uuidArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Deve encontrar o usuario por ID com sucesso quando o optional está vazio")
+        void shouldGetUserByIdWithSuccessWhenOptionalIsEmpty() {
+
+            //Arrange
+
+            var userId = UUID.randomUUID();
+
+            doReturn(Optional.empty()).when(userRepository).findById(uuidArgumentCaptor.capture());
+
+            //Act
+
+            var output = userService.getUserById(userId.toString());
+
+            //Assert
+
+            assertTrue(output.isEmpty());
+            assertEquals(userId, uuidArgumentCaptor.getValue());
+        }
+    }
+
+    @Nested
+    class listUser {
+
+        @Test
+        @DisplayName("Deve retornar todos os usuarios com sucesso")
+        void shouldReturnAllUsersWithSuccess() {
+            //Arrange
+            var user = new User(
+                    UUID.randomUUID(),
+                    "usernameteste",
+                    "teste@email.com",
+                    "senhateste123",
+                    Instant.now(),
+                    null
+            );
+            var userList = List.of(user);
+            doReturn(List.of(user)).when(userRepository).findAll();
+            //Act
+
+            var output = userService.listUsers();
+
+            //Assert
+
+            assertNotNull(output);
+            assertEquals(userList.size(), output.size());
+
+        }
+    }
+
+    @Nested
+    class deleteById {
+
+        @Test
+        @DisplayName("Deve deletar um usuario com sucesso")
+        void shouldDeleteUserWithSuccess() {
+            //Arrange
+            var user = new User(
+                    UUID.randomUUID(),
+                    "usernameteste",
+                    "teste@email.com",
+                    "senhateste123",
+                    Instant.now(),
+                    null
+            );
+
+            doReturn(true).when(userRepository).existsById(uuidArgumentCaptor.capture());
+
+            doNothing().when(userRepository).deleteById(uuidArgumentCaptor.capture());
+
+            var userId = UUID.randomUUID();
+            //Act
+
+            userService.deleteById(userId.toString());
+
+            //Assert
+            var idList = uuidArgumentCaptor.getAllValues();
+
+            assertEquals(userId, idList.get(0));
+            assertEquals(userId, idList.get(1));
+            assertEquals(userId, uuidArgumentCaptor.getValue());
 
         }
     }

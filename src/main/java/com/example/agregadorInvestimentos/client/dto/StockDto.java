c531979a288e7bb99138d99009e5e391aabc0ef8
BrunoGuilhermeS <86699309+BrunoGuilhermeS@users.noreply.github.com>
2024-07-01 23:05:01 -0300

    Brapi api

---
+++ src/main/java/com/example/agregadorInvestimentos/client/dto/StockDto.java
@@ -0,0 +1,4 @@
+package com.example.agregadorInvestimentos.client.dto;
+
+public record StockDto(double regularMarketPrice) {
+}
--- src/main/java/com/example/agregadorInvestimentos/service/AccountService.java
+++ src/main/java/com/example/agregadorInvestimentos/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.example.agregadorInvestimentos.service;
 
+import com.example.agregadorInvestimentos.client.BrapiClient;
 import com.example.agregadorInvestimentos.controller.dto.AccountStockResponseDto;
 import com.example.agregadorInvestimentos.controller.dto.AssociateAccountStockDto;
 import com.example.agregadorInvestimentos.entity.AccountStock;
@@ -7,6 +8,7 @@ import com.example.agregadorInvestimentos.entity.StockAccountId;
 import com.example.agregadorInvestimentos.repository.AccountRepository;
 import com.example.agregadorInvestimentos.repository.AccountStockRepository;
 import com.example.agregadorInvestimentos.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,11 +19,15 @@ import java.util.UUID;
 @Service
 public class AccountService {
 
+    @Value("#{environment.TOKEN}")
+    private String TOKEN;
     private AccountRepository accountRepository;
 
     private StockRepository stockRepository;
 
     private AccountStockRepository accountStockRepository;
+
+    private BrapiClient brapiClient;
 
     public AccountService(AccountRepository accountRepository, StockRepository stockRepository, AccountStockRepository accountStockRepository) {
         this.accountRepository = accountRepository;
@@ -56,7 +62,17 @@ public class AccountService {
         var account = accountRepository.findById(UUID.fromString(accountId))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return account.getAccountStocks()
-                .stream().map(as -> new AccountStockResponseDto(as.getStock().getStockId(), as.getQuantity(), 0.0))
+                .stream().map(as -> new AccountStockResponseDto(
+                        as.getStock().getStockId(),
+                        as.getQuantity(), getTotal(as.getQuantity(), as.getStock().getStockId())))
                 .toList();
+    }
+
+    private double getTotal(Integer quantity, String stockId) {
+
+        var response = brapiClient.getQuote(TOKEN, stockId);
+        var price = response.results().getFirst().regularMarketPrice();
+
+        return quantity * price;
     }
 }
